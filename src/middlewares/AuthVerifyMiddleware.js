8c804b3634fd3f0586afa20821c48b9fd0c6febd
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-12-26 23:16:25 +0600

    2nd commit

---
+++ src/middlewares/AuthVerifyMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+module.exports = (req,res,next)=>{
+    let Token = req.headers['token'];
+    jwt.verify(Token,"SecretKey457246",function (error,decoded) {
+        if(error){
+            console.log(Token)
+            res.status(401).json({status:"unauthorized"})
+        }
+        else {
+            let email = decoded['data'];
+            console.log(email)
+            req.headers.email = email
+            next();
+        }
+    })
+}
